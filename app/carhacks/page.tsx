1bad64cdeb178b0df0a58b251f39d15f23bb19b5
Reno117 <parkerrenoestes@gmail.com>
2023-09-08 20:53:30 -0400

    Added textbox component to carolina hacks page

--- app/carhacks/page.tsx
+++ app/carhacks/page.tsx
@@ -1,9 +1,12 @@
 import Link from 'next/link';
+import ColoredTextBlock from '../components/ColoredTextBlock';
+
 
 export default function carhacks() {
   return (
     <>
       <h1>This is my experience hosting my local hackathon, Carolina Hacks!</h1>
+      <ColoredTextBlock />
       <h2>
         <Link href="/" className='back'>Back to home</Link>
       </h2>
