db95cc9cc123f96ecd674f115e148573d4d50fc9
Parker <parkerrenoestes@gmail.com>
2023-09-19 15:56:40 -0400

    Added text content to pages and fixed text formatting

--- app/carhacks/page.tsx
+++ app/carhacks/page.tsx
@@ -6,18 +6,16 @@ import { UIEventHandler, useState } from 'react';
 
 
 export default function carhacks() {
-  const [position, setPosition] = useState(0);
-  const onScroll: UIEventHandler<HTMLDivElement> = (event) => {
-    const pi =event.currentTarget.scrollTop;
-    setPosition(pi)
-  }
+  // const [position, setPosition] = useState(0);
+  // const onScroll: UIEventHandler<HTMLDivElement> = (event) => {
+  //   const pi =event.currentTarget.scrollTop;
+  //   setPosition(pi)
+  // }
   return (
-    <div onScroll={onScroll} className='max-h-[80vh] overflow-y-scroll'>
+    <div>
       <h1>This is my experience hosting my local hackathon, Carolina Hacks!</h1>
       <ColoredTextBlock />
-      <div>
-        {position}
-      </div>
+      
       <h2>
         <Link href="/" className="fixed bottom-0 left-0 w-full bg-gray-900 text-white p-2">Back to home</Link>
       </h2>
