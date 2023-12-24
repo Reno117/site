bda397d3d35ec4bf772e18440dc1c111ee82f349
Parker <parkerrenoestes@gmail.com>
2023-12-24 16:18:49 -0500

    trying to fix ' errors

--- app/components/ClemsonCourtsDescript.tsx
+++ app/components/ClemsonCourtsDescript.tsx
@@ -4,18 +4,19 @@ const ClemsonCourtsDescript = () => {
   return (
     <div className="justify-start relative max-w-[600px] flex-grow mb-20 bg-[#294b5a] text-white p-5 my-0 flex">
      <div className="max-h-[100%] max-w-[100%] overflow-auto">
-      <p>
-  While still in development, "Clemson Courts" is my first step into full-stack coding, 
-  aiming to cultivate an inclusive ecosystem for basketball enthusiasts at my university's courts. 
+      
+<p>
+  While still in development, &quot;Clemson Courts&quot; is my first step into full-stack coding, 
+  aiming to cultivate an inclusive ecosystem for basketball enthusiasts at my university&apos;s courts. 
   Actively engaging both back-end and front-end facets, this project represents a blend of my 
   passions—coding and basketball. The initiative thrives on collaboration, as I encourage friends and 
   fellow basketball players to utilize the platform, facilitating communication about game schedules. 
   As this real-time, collaborative effort evolves, I find myself not only immersed in the technical 
   intricacies of full-stack development but also witnessing the practical fusion of my coding skills 
-  with my love for basketball. "Clemson Courts" is not just a project; it's a dynamic space where my 
+  with my love for basketball. &quot;Clemson Courts&quot; is not just a project; it&apos;s a dynamic space where my 
   hobbies converge, and learning becomes a shared experience among the community of players.
-
 </p>
+
 
       </div>
     </div>
