a4e5d82239c810695ffae709be03c75579684189
Reno117 <parkerrenoestes@gmail.com>
2024-01-05 00:34:28 -0500

    chanaged some text blocks

--- app/components/CarHacksDescript.tsx
+++ app/components/CarHacksDescript.tsx
@@ -5,7 +5,7 @@ const CarHacksDescript = () => {
     <div className="items-center relative max-w-[600px] flex-grow bg-[#294b5a] text-white p-5 my-0 flex">
       <div className="max-h-[100%] max-w-[100%] overflow-auto">
         <p>
-          Organizing Carolina Hacks, a student-led high school hackathon, was a
+          Helping organize Carolina Hacks, a student-led high school hackathon, was a
           transformative experience that significantly influenced my growth as a
           leader in the coding community. Coordinating the event sharpened my
           technical skills, enhanced my problem-solving abilities, and deepened
--- app/page.tsx
+++ app/page.tsx
@@ -6,6 +6,7 @@ import CarHacksDescript from "./components/CarHacksDescript";
 import PortfolioDescript from "./components/PortfolioDescript";
 import ClemsonCourtsDescript from "./components/ClemsonCourtsDescript";
 
+
 export default function Home() {
   const scrollToSection = (sectionId: string, event: React.MouseEvent) => {
     event.preventDefault();
@@ -66,9 +67,27 @@ export default function Home() {
           <h2 className="text-3xl font-bold mb-20">Projects</h2>
         </div>
         <div className="flex justify-center">
-        <div className="border-[#000000] border-b-[2px] w-14 " />
+          <div className="border-[#000000] border-b-[2px] w-14 " />
         </div>
-          
+
+        <h1 className="text-3xl mb-2 text-center ml-4 sm:text-3xl md:text-4xl lg:text-5xl xl:text-6xl">
+          Clemson Courts
+        </h1>
+
+        <div className="mb-10 flex justify-center">
+          <div className="items-center mt-2 mb-8 grid grid-cols-1 md:grid-cols-2 gap-16 max-w-[1200px]">
+            <div className="flex-grow">
+              
+                <img
+                  src="./courts.jpg_large"
+                  alt="logo"
+                  className="w-full object-cover transition-transform transform hover:scale-110"
+                />
+              
+            </div>
+            <ClemsonCourtsDescript />
+          </div>
+        </div>
 
         <h1 className="text-3xl mb-2 text-center ml-4 sm:text-3xl md:text-4xl lg:text-5xl xl:text-6xl">
           Carolina Hacks
@@ -77,11 +96,11 @@ export default function Home() {
         <div className="mb-10 flex justify-center">
           <div className="items-center mt-2 mb-8 grid grid-cols-1 md:grid-cols-2 gap-16 max-w-[1200px]">
             <div className="flex-grow">
-            <img 
-              src="./car_hacks.png"
-              alt="logo"
-              className="w-full object-cover transition-transform transform hover:scale-110"
-            />
+              <img
+                src="./car_hacks.png"
+                alt="logo"
+                className="w-full object-cover transition-transform transform hover:scale-110"
+              />
             </div>
             <CarHacksDescript />
           </div>
@@ -94,30 +113,13 @@ export default function Home() {
         <div className="mb-10 flex justify-center">
           <div className="items-center mt-2 mb-8 grid grid-cols-1 md:grid-cols-2 gap-16 max-w-[1200px]">
             <div className="flex-grow">
-            <img 
-              src="./nexjts-react.png"
-              alt="logo"
-              className="w-full object-cover transition-transform transform hover:scale-110"
-            />
+              <img
+                src="./nexjts-react.png"
+                alt="logo"
+                className="w-full object-cover transition-transform transform hover:scale-110"
+              />
             </div>
             <PortfolioDescript />
-          </div>
-        </div>
-
-        <h1 className="text-3xl mb-2 text-center ml-4 sm:text-3xl md:text-4xl lg:text-5xl xl:text-6xl">
-          Clemson Courts
-        </h1>
-
-        <div className="mb-10 flex justify-center">
-          <div className="items-center mt-2 mb-8 grid grid-cols-1 md:grid-cols-2 gap-16 max-w-[1200px]">
-            <div className="flex-grow">
-            <img 
-              src="./courts.jpg_large"
-              alt="logo"
-              className="w-full object-cover transition-transform transform hover:scale-110"
-            />
-            </div>
-            <ClemsonCourtsDescript />
           </div>
         </div>
       </div>
