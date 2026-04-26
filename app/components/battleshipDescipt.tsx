95edbb9bbd752aaaf5894f261b56e40af3878b03
Parker <parkerrenoestes@gmail.com>
2026-04-26 15:09:41 -0400

    Addded battleship to site

--- app/components/battleshipDescipt.tsx
+++ app/components/battleshipDescipt.tsx
@@ -1,14 +1,14 @@
 import React from "react";
 import GitHubButton from "./GitHubButton";
 
-const BattleshipDescript= () => {
+const battleshipDescript= () => {
   return (
     <div className="items-center relative max-w-[600px] flex-grow bg-[#294b5a] text-white p-5 my-0 flex">
       <div className="max-h-[100%] max-w-[100%] overflow-auto">
       
 <p>
     Battleship is a full stack website developed with Next.js and hosted on Vercel. This project required
-    us to make multiple games at a time, where we can connect to other team's servers. we also had to 
+    us to make multiple games at a time, where we can connect to other teams servers. we also had to 
     create a working frontend to display the battleship game, which updated in real time, showing current
     game stats and board progress.
 
@@ -22,5 +22,5 @@ const BattleshipDescript= () => {
   );
 };
 
-export default BattleshipDescript;
+export default battleshipDescript;
 
